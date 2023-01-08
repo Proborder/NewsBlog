a1015bf13243aca043bfd7824845d6a737d3b4ee
Max <minotepas@mail.ru>
2023-01-08 14:07:23 +0300

    Мелочи

--- mysite/news_app/admin.py
+++ mysite/news_app/admin.py
@@ -12,4 +12,5 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'created_at', 'updated_at', 'is_published']
+    list_editable = ['is_published']
     prepopulated_fields = {'slug': ['title']}
